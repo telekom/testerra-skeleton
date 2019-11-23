2793569e6070a84124fb5561b1da4ed03a8ac82a
mreiche <mike.reiche@t-systems.com>
2019-11-23 09:19:32 +0100

    Updated Factory example

--- inject-factories/src/main/java/factories/MyPageFactory.java
+++ inject-factories/src/main/java/factories/MyPageFactory.java
@@ -2,13 +2,13 @@ package factories;
 
 import eu.tsystems.mms.tic.testframework.logging.Loggable;
 import eu.tsystems.mms.tic.testframework.pageobjects.DefaultPageFactory;
-import eu.tsystems.mms.tic.testframework.pageobjects.IPage;
-import eu.tsystems.mms.tic.testframework.pageobjects.IPageFactory;
+import eu.tsystems.mms.tic.testframework.pageobjects.PageObject;
+import eu.tsystems.mms.tic.testframework.pageobjects.PageObjectFactory;
 import org.openqa.selenium.WebDriver;
 
-public class MyPageFactory extends DefaultPageFactory implements IPageFactory, Loggable {
+public class MyPageFactory extends DefaultPageFactory implements PageObjectFactory, Loggable {
     @Override
-    public <T extends IPage> T createPage(Class<T> pageClass, WebDriver webDriver) {
+    public <T extends PageObject> T createPage(Class<T> pageClass, WebDriver webDriver) {
         log().info("Enjoy your custom Page");
         return super.createPage(pageClass, webDriver);
     }
--- inject-factories/src/test/java/test/FactoriesTest.java
+++ inject-factories/src/test/java/test/FactoriesTest.java
@@ -3,13 +3,11 @@ package test;
 import eu.tsystems.mms.tic.testframework.testing.TesterraTest;
 import factories.MyPageFactory;
 import org.testng.annotations.Test;
-import pages.StartPage;
 
 public class FactoriesTest extends TesterraTest {
     @Test
     public void test_PageFactory() {
         Assert.assertTrue(pageFactory instanceof MyPageFactory);
-        pageFactory.createPage(StartPage.class);
     }
 
 }
