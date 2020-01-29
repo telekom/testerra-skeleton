46f456804addfd8d98976c362a715b18cab54185
mreiche <mike.reiche@t-systems.com>
2020-01-29 11:08:40 +0100

    Made Threadsafe example

--- google-search/src/test/java/test/GoogleSearchTest.java
+++ google-search/src/test/java/test/GoogleSearchTest.java
@@ -1,22 +1,15 @@
 package test;
 
 import eu.tsystems.mms.tic.testframework.testing.TesterraTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pages.ResultPage;
 import pages.StartPage;
 
 public class GoogleSearchTest extends TesterraTest {
 
-    private StartPage startPage;
-
-    @BeforeClass
-    private void prepare() {
-        startPage = pageFactory.createPage(StartPage.class);
-    }
-
-    @Test
+    @Test(invocationCount = 1, threadPoolSize = 1)
     public void test_Search() {
+        StartPage startPage = pageFactory.createPage(StartPage.class);
         ResultPage resultPage = startPage.type("testerra").searchByEnter();
         resultPage.result().list().first().headline().text().contains("Testerra");
     }
