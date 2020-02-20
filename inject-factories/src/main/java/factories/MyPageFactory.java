7286645fe1b436bc2eb6cedc81147b2708b83a06
mreiche <mike.reiche@t-systems.com>
2020-02-20 18:41:49 +0100

    Removed useless interface declaration

--- inject-factories/src/main/java/factories/MyPageFactory.java
+++ inject-factories/src/main/java/factories/MyPageFactory.java
@@ -3,10 +3,9 @@ package factories;
 import eu.tsystems.mms.tic.testframework.logging.Loggable;
 import eu.tsystems.mms.tic.testframework.pageobjects.DefaultPageFactory;
 import eu.tsystems.mms.tic.testframework.pageobjects.PageObject;
-import eu.tsystems.mms.tic.testframework.pageobjects.PageObjectFactory;
 import org.openqa.selenium.WebDriver;
 
-public class MyPageFactory extends DefaultPageFactory implements PageObjectFactory, Loggable {
+public class MyPageFactory extends DefaultPageFactory implements Loggable {
     @Override
     public <T extends PageObject> T createPage(Class<T> pageClass, WebDriver webDriver) {
         log().info("Enjoy your custom Page");
