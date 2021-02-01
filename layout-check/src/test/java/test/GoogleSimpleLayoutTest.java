52eca1b167d7b02252a306cc0cf4d7431150e206
mreiche <mike.reiche@t-systems.com>
2021-02-01 10:46:31 +0100

    API updates

--- layout-check/src/test/java/test/GoogleSimpleLayoutTest.java
+++ layout-check/src/test/java/test/GoogleSimpleLayoutTest.java
@@ -1,6 +1,5 @@
 package test;
 
-import eu.tsystems.mms.tic.testframework.pageobjects.Aria;
 import eu.tsystems.mms.tic.testframework.pageobjects.LocatorFactoryProvider;
 import eu.tsystems.mms.tic.testframework.pageobjects.UiElement;
 import eu.tsystems.mms.tic.testframework.pageobjects.UiElementFinder;
@@ -16,15 +15,15 @@ public class GoogleSimpleLayoutTest extends AbstractGoogleTest implements Locato
         UiElement searchBtn = finder.find(Locate.by(By.name("btnK")).displayed());
         UiElement luckyBtn = finder.find(Locate.by(By.name("btnI")).displayed());
 
-        searchBtn.expectThat().bounds().below(searchInput).is(true);
-        luckyBtn.expectThat().bounds().rightOf(searchBtn).is(true);
-        luckyBtn.expectThat().bounds().fromTop().toTopOf(searchBtn).is(0);
-        luckyBtn.expectThat().bounds().intersects(searchBtn).is(false);
+        searchBtn.expect().bounds().below(searchInput).is(true);
+        luckyBtn.expect().bounds().rightOf(searchBtn).is(true);
+        luckyBtn.expect().bounds().fromTop().toTopOf(searchBtn).is(0);
+        luckyBtn.expect().bounds().intersects(searchBtn).is(false);
     }
 
     @Test
     public void test_Layout_Image() {
         UiElementFinder finder = getFinder();
-        finder.find(By.id("body")).expectThat().screenshot().pixelDistance("GoogleBody").isLowerThan(10);
+        finder.find(By.id("body")).expect().screenshot().pixelDistance("GoogleBody").isLowerThan(10);
     }
 }
