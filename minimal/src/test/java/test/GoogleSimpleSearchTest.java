10cd422d317949d7c06f127d7bec2618794a5287
Mike Reiche <mike.reiche@t-systems.com>
2021-10-22 12:31:40 +0200

    Fixed minimal example

--- minimal/src/test/java/test/GoogleSimpleSearchTest.java
+++ minimal/src/test/java/test/GoogleSimpleSearchTest.java
@@ -1,13 +1,18 @@
 package test;
 
+import eu.tsystems.mms.tic.testframework.pageobjects.Locator;
+import eu.tsystems.mms.tic.testframework.pageobjects.LocatorFactoryProvider;
 import eu.tsystems.mms.tic.testframework.pageobjects.UiElementFinder;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-public class GoogleSimpleSearchTest extends AbstractGoogleTest {
+public class GoogleSimpleSearchTest extends AbstractGoogleTest implements LocatorFactoryProvider {
 
     @Test()
     public void test_Search() {
+        // Always find displayed elements
+        LOCATE.setConfigurator(Locator::displayed);
+
         UiElementFinder finder = createFinder();
         finder.find(By.name("q")).type("Hallo World");
         finder.find(By.name("btnK")).click();
