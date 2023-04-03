3683346f5d14fc2d508f8e3644a08050ed7657eb
Martin Großmann <martin.grossmann@t-systems.com>
2023-04-03 17:15:14 +0200

    Reformat AbstractTest

--- src/test/java/eu/tsystems/mms/testerra/demo/AbstractTest.java
+++ src/test/java/eu/tsystems/mms/testerra/demo/AbstractTest.java
@@ -25,25 +25,21 @@ import eu.tsystems.mms.tic.testframework.constants.Browsers;
 import eu.tsystems.mms.tic.testframework.testing.TesterraTest;
 import eu.tsystems.mms.tic.testframework.testing.WebDriverManagerProvider;
 import eu.tsystems.mms.tic.testframework.useragents.ChromeConfig;
-import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverManager;
-import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverProxyUtils;
-import org.openqa.selenium.Proxy;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.CapabilityType;
 import org.testng.annotations.BeforeSuite;
 
-public class AbstractTest extends TesterraTest implements WebDriverManagerProvider{
-    /**
-     * https://tapas-docs.s3.eu-central-1.amazonaws.com/testerra/latest/index.html#_chrome_in_a_container
-     */
+public class AbstractTest extends TesterraTest implements WebDriverManagerProvider {
+
     @BeforeSuite
     public void configureChromeOptions() {
+
         WEB_DRIVER_MANAGER.setUserAgentConfig(Browsers.chromeHeadless, new ChromeConfig() {
             @Override
             public void configure(ChromeOptions options) {
                 options.addArguments("--disable-dev-shm-usage");
             }
         });
+
     }
 
 }
